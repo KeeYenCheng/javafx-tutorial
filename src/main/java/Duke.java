72bac6e8047a1c962fe5727944c41d2ed65ea093
YC <yencheng109@gmail.com>
2026-09-03 22:46:31 +0800

    Tutorial-Part3

---
+++ src/main/java/Duke.java
@@ -0,0 +1,9 @@
+public class Duke {
+    public static void main(String[] args) {
+        System.out.println("hello"); 
+    }
+
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,6 +23,18 @@ public class Main extends Application {
     private Button sendButton;
     private Scene scene;
 
+    private Duke duke = new Duke();
+    private void handleUserInput() {
+        String userText = userInput.getText();
+        String dukeText = duke.getResponse(userInput.getText());
+
+        dialogContainer.getChildren().addAll(
+            DialogBox.getUserDialog(userText, userImage),
+            DialogBox.getDukeDialog(dukeText, dukeImage)
+        );
+
+        userInput.clear();
+    }
     @Override
     public void start(Stage stage) {
 
@@ -32,10 +44,9 @@ public class Main extends Application {
 
         userInput = new TextField();
         sendButton = new Button("Send");
-
-        DialogBox dialogBox = new DialogBox("Hello!", userImage);
-        dialogContainer.getChildren().addAll(dialogBox);
-
+        
+        sendButton.setOnMouseClicked(event -> handleUserInput());
+        userInput.setOnAction((event) -> handleUserInput()); 
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
@@ -67,7 +78,7 @@ public class Main extends Application {
         AnchorPane.setLeftAnchor(userInput, 1.0);
         AnchorPane.setBottomAnchor(userInput, 1.0);
 
-
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
         stage.setScene(scene);
         stage.show();
     }
